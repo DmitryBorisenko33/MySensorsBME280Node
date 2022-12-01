e63ce592917a3654189ddf17d5e858d15afc2f38
Dmitry Borisenko <67171972+IoTManagerProject@users.noreply.github.com>
2022-12-01 22:09:06 +0100

    ignor

--- src/pinMap.h
+++ src/pinMap.h
@@ -18,7 +18,7 @@
 #define _VARIANT_NRF52_DK_
 
 /** Master clock frequency */
-#define VARIANT_MCK       (64000000ul)
+#define VARIANT_MCK (64000000ul)
 
 /*----------------------------------------------------------------------------
  *        Headers
@@ -27,83 +27,82 @@
 #include "WVariant.h"
 
 #ifdef __cplusplus
-extern "C"
-{
-#endif // __cplusplus
+extern "C" {
+#endif  // __cplusplus
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (26u)
-#define NUM_DIGITAL_PINS     (20u)
-#define NUM_ANALOG_INPUTS    (8u)
-#define NUM_ANALOG_OUTPUTS   (0u)
+#define PINS_COUNT (26u)
+#define NUM_DIGITAL_PINS (20u)
+#define NUM_ANALOG_INPUTS (8u)
+#define NUM_ANALOG_OUTPUTS (0u)
 
 // LEDs
-#define PIN_LED1                (6)
-#define PIN_LED2                (7)
-#define PIN_LED3                (8)
-#define PIN_LED4                (9)
-#define LED_BUILTIN             PIN_LED1
+#define PIN_LED1 (6)
+#define PIN_LED2 (7)
+#define PIN_LED3 (8)
+#define PIN_LED4 (9)
+#define LED_BUILTIN PIN_LED1
 
 // Buttons
-#define PIN_BUTTON1             (2)
-#define PIN_BUTTON2             (3)
-#define PIN_BUTTON3             (4)
-#define PIN_BUTTON4             (5)
+#define PIN_BUTTON1 (2)
+#define PIN_BUTTON2 (3)
+#define PIN_BUTTON3 (4)
+#define PIN_BUTTON4 (5)
 
 /*
  * Analog pins
  */
-#define PIN_A0               (14)
-#define PIN_A1               (15)
-#define PIN_A2               (16)
-#define PIN_A3               (17)
-#define PIN_A4               (18)
-#define PIN_A5               (19)
-#define PIN_A6               (20) /* AIN3 (P0.05)        */
-#define PIN_A7               (21) /* AIN0 (P0.02) / AREF */
+#define PIN_A0 (14)
+#define PIN_A1 (15)
+#define PIN_A2 (16)
+#define PIN_A3 (17)
+#define PIN_A4 (18)
+#define PIN_A5 (19)
+#define PIN_A6 (20) /* AIN3 (P0.05)        */
+#define PIN_A7 (21) /* AIN0 (P0.02) / AREF */
 
-static const uint8_t A0  = PIN_A0 ; // AIN1
-static const uint8_t A1  = PIN_A1 ; // AIN2
-static const uint8_t A2  = PIN_A2 ; // AIN4
-static const uint8_t A3  = PIN_A3 ; // AIN5
-static const uint8_t A4  = PIN_A4 ; // AIN6
-static const uint8_t A5  = PIN_A5 ; // AIN7
-static const uint8_t A6  = PIN_A6 ; // AIN3 (P0.05)
-static const uint8_t A7  = PIN_A7 ; // AIN0 (P0.02) / AREF
-#define ADC_RESOLUTION    14
+static const uint8_t A0 = PIN_A0;  // AIN1
+static const uint8_t A1 = PIN_A1;  // AIN2
+static const uint8_t A2 = PIN_A2;  // AIN4
+static const uint8_t A3 = PIN_A3;  // AIN5
+static const uint8_t A4 = PIN_A4;  // AIN6
+static const uint8_t A5 = PIN_A5;  // AIN7
+static const uint8_t A6 = PIN_A6;  // AIN3 (P0.05)
+static const uint8_t A7 = PIN_A7;  // AIN0 (P0.02) / AREF
+#define ADC_RESOLUTION 14
 
 // Other pins
-#define PIN_AREF           (21)
+#define PIN_AREF (21)
 static const uint8_t AREF = PIN_AREF;
 
 /*
  * Serial interfaces
  */
 // Serial
-#define PIN_SERIAL_RX       (24)
-#define PIN_SERIAL_TX       (25)
+#define PIN_SERIAL_RX (12)
+#define PIN_SERIAL_TX (11)
 
 /*
  * SPI Interfaces
  */
 #define SPI_INTERFACES_COUNT 1
 
-#define PIN_SPI_MISO         (12)
-#define PIN_SPI_MOSI         (11)
-#define PIN_SPI_SCK          (13)
+#define PIN_SPI_MISO (12)
+#define PIN_SPI_MOSI (11)
+#define PIN_SPI_SCK (13)
 
-static const uint8_t SS   = 10 ;
-static const uint8_t MOSI = PIN_SPI_MOSI ;
-static const uint8_t MISO = PIN_SPI_MISO ;
-static const uint8_t SCK  = PIN_SPI_SCK ;
+static const uint8_t SS = 10;
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK = PIN_SPI_SCK;
 
 /*
  * Wire Interfaces
  */
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA         (22u)
-#define PIN_WIRE_SCL         (23u)
+#define PIN_WIRE_SDA (22u)
+#define PIN_WIRE_SCL (23u)
 
 static const uint8_t SDA = PIN_WIRE_SDA;
 static const uint8_t SCL = PIN_WIRE_SCL;
@@ -111,7 +110,7 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 /*
  * Reset Button at P0.21
  */
-#define RESET_PIN            21
+#define RESET_PIN 21
 
 #ifdef __cplusplus
 }
